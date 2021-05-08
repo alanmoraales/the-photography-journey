f55a365e3bed4cad28ee36964204c221680854a9
Alan Morales <alanmoraales845@gmail.com>
2021-05-08 12:54:38 -0500

    setup mdx

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>The Photography Journey</title>
+      <meta name="description" content="A photography blog" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main>The Photography Journey</main>
+  </div>
+)
+
+export default Home
