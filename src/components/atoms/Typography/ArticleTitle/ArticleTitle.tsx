874b2539cd8f23fe133ef0acf7a5bc736c71b4f9
Alan Morales <alanmoraales845@gmail.com>
2021-07-11 15:51:57 -0500

    remove ArticleTitle component

--- src/components/atoms/Typography/ArticleTitle/ArticleTitle.tsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled'
-import { Heading } from '@chakra-ui/react'
-
-const ArticleTitle = styled(Heading)`
-  font-size: 2.488em;
-  font-weight: inherit;
-`
-
-export default ArticleTitle
