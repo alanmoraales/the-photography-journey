e4134f1748e87c5b8cdddc727f1aa6bf3c84ecdd
Alan Morales <alanmoraales845@gmail.com>
2021-07-13 23:49:38 -0500

    trying out body component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 import styled from '@emotion/styled'
 import Head from 'components/atoms/Head'
-import { Heading } from 'components/atoms/Typography'
+import { Heading, Body } from 'components/atoms/Typography'
 
 const Container = styled.div`
   padding: 25px;
+  max-width: 75ch;
+  margin: 0 auto;
+`
+
+const Header = styled.div`
+  padding-top: 64px;
+  gap: 8px;
+  display: grid;
+`
+
+const Meta = styled.div`
+  display: flex;
+  gap: 9px;
+`
+
+const Content = styled.div`
+  padding-top: 32px;
 `
 
 const Home = () => (
@@ -11,7 +28,26 @@ const Home = () => (
     <Head />
     <main>
       <Container>
-        <Heading>Starting The Journey</Heading>
+        <Header>
+          <Heading>Starting The Journey</Heading>
+          <Meta>
+            <Body color="primary">Post</Body>
+            <Body color="opaque" weight="light">
+              •
+            </Body>
+            <Body color="opaque" weight="light">
+              June 25, 2021
+            </Body>
+          </Meta>
+        </Header>
+        <Content>
+          <Body variant="blogParagraph">
+            The idea of the two halves of life was popularized by Carl Jung, a
+            psychologist, also known as founder of the school of analytical
+            psychology. It says that a human life, like a day, has its morning
+            and its evening, each one with its tasks and goals.{' '}
+          </Body>
+        </Content>
       </Container>
     </main>
   </div>
