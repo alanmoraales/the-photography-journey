fea680eddef4f316847e2faff8571f46bc10bf36
Alan Morales <alanmoraales845@gmail.com>
2021-07-13 23:49:20 -0500

    add body component and base typography attributes

---
+++ src/components/atoms/Typography/Body/Body.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react'
+import { Text } from '@chakra-ui/react'
+import { GetEnumKeys } from 'utils/common'
+import { EBodyVariants } from 'shared/types/design'
+import { IBaseTypography, typographyColors } from '../IBaseTypography'
+
+const bodyWeights = {
+  light: 'light',
+  normal: 'normal',
+  medium: 'medium',
+}
+
+interface IBody extends IBaseTypography {
+  variant?: GetEnumKeys<typeof EBodyVariants>
+  weight?: keyof typeof bodyWeights
+}
+
+const Body: FC<IBody> = ({
+  children,
+  variant = 'paragraph',
+  color = 'normal',
+  weight = 'normal',
+}) => (
+  <Text fontSize={variant} fontWeight={weight} color={typographyColors[color]}>
+    {children}
+  </Text>
+)
+
+export { Body }
