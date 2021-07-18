2f8a362903bcfb7f62a89df9e29cf9df6b2fd1f8
Alan Morales <alanmoraales845@gmail.com>
2021-07-18 15:12:40 -0500

    add ArticleHeader component

--- src/components/molecules/Article/ArticleHeader/ArticleHeader.tsx
+++ src/components/molecules/Article/ArticleHeader/ArticleHeader.tsx
@@ -1,7 +1,57 @@
+import { Box } from '@chakra-ui/react'
+import { BackArrow, LoveIcon, ShareIcon } from 'components/atoms/Icons'
+import { Heading, Body } from 'components/atoms/Typography'
+import { EArticleType } from 'shared/types/article'
+import { GetEnumKeys } from 'utils/common'
+
 interface IArticleHeader {
   title: string
+  type?: GetEnumKeys<typeof EArticleType>
+  date: string
 }
 
-const ArticleHeader = () => <div>header</div>
+const ArticleHeader = ({ title, type = 'Post', date }: IArticleHeader) => (
+  <Box
+    bg="glass"
+    display="grid"
+    placeItems="center"
+    position="sticky"
+    top="0px"
+    style={{ backdropFilter: 'blur(12px)' }}
+  >
+    <Box
+      display="grid"
+      padding="6"
+      maxWidth="75ch"
+      style={{ gap: '32px' }}
+      width="100%"
+    >
+      <Box display="grid" style={{ gap: '8px' }}>
+        <Heading>{title}</Heading>
+        <Box display="flex" style={{ gap: '8px' }}>
+          <Body color="primary">{type}</Body>
+          <Body color="opaque" weight="light">
+            •
+          </Body>
+          <Body color="opaque" weight="light">
+            {date}
+          </Body>
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Box display="flex" style={{ gap: '8px' }} alignItems="center">
+          <BackArrow />
+          <Heading variant="h6" textDecoration="underline">
+            Go back
+          </Heading>
+        </Box>
+        <Box display="flex" style={{ gap: '16px' }} alignItems="center">
+          <ShareIcon boxSize="21px" />
+          <LoveIcon boxSize="21px" />
+        </Box>
+      </Box>
+    </Box>
+  </Box>
+)
 
 export { ArticleHeader }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,17 @@
 import styled from '@emotion/styled'
 import Head from 'components/atoms/Head'
-import { Heading, Body } from 'components/atoms/Typography'
+import { ArticleHeader } from 'components/molecules/Article'
+import { Body } from 'components/atoms/Typography'
+import { Image } from '@chakra-ui/react'
+
+const Main = styled.main`
+  padding-top: 64px;
+`
 
 const Container = styled.div`
-  padding: 25px;
+  padding: 24px;
   max-width: 75ch;
   margin: 0 auto;
-`
-
-const Header = styled.div`
-  padding-top: 64px;
-  gap: 8px;
-  display: grid;
-`
-
-const Meta = styled.div`
-  display: flex;
-  gap: 9px;
 `
 
 const Content = styled.div`
@@ -24,33 +19,55 @@ const Content = styled.div`
 `
 
 const Home = () => (
-  <div>
+  <>
     <Head />
-    <main>
+    <Main>
+      <ArticleHeader title="Starting The Journey" date="June 25, 2021" />
       <Container>
-        <Header>
-          <Heading>Starting The Journey</Heading>
-          <Meta>
-            <Body color="primary">Post</Body>
-            <Body color="opaque" weight="light">
-              •
-            </Body>
-            <Body color="opaque" weight="light">
-              June 25, 2021
-            </Body>
-          </Meta>
-        </Header>
         <Content>
+          <Image
+            marginBottom="8"
+            alt="Jarana yucateca"
+            src="https://images.unsplash.com/photo-1606971220186-e18b2425247b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
+          />
           <Body variant="blogParagraph">
             The idea of the two halves of life was popularized by Carl Jung, a
             psychologist, also known as founder of the school of analytical
             psychology. It says that a human life, like a day, has its morning
-            and its evening, each one with its tasks and goals.{' '}
+            and its evening, each one with its tasks and goals. The idea of the
+            two halves of life was popularized by Carl Jung, a psychologist,
+            also known as founder of the school of analytical psychology. It
+            says that a human life, like a day, has its morning and its evening,
+            each one with its tasks and goals. The idea of the two halves of
+            life was popularized by Carl Jung, a psychologist, also known as
+            founder of the school of analytical psychology. It says that a human
+            life, like a day, has its morning and its evening, each one with its
+            tasks and goals. The idea of the two halves of life was popularized
+            by Carl Jung, a psychologist, also known as founder of the school of
+            analytical psychology. It says that a human life, like a day, has
+            its morning and its evening, each one with its tasks and goals.{' '}
+          </Body>
+          <Body variant="blogParagraph">
+            The idea of the two halves of life was popularized by Carl Jung, a
+            psychologist, also known as founder of the school of analytical
+            psychology. It says that a human life, like a day, has its morning
+            and its evening, each one with its tasks and goals. The idea of the
+            two halves of life was popularized by Carl Jung, a psychologist,
+            also known as founder of the school of analytical psychology. It
+            says that a human life, like a day, has its morning and its evening,
+            each one with its tasks and goals. The idea of the two halves of
+            life was popularized by Carl Jung, a psychologist, also known as
+            founder of the school of analytical psychology. It says that a human
+            life, like a day, has its morning and its evening, each one with its
+            tasks and goals. The idea of the two halves of life was popularized
+            by Carl Jung, a psychologist, also known as founder of the school of
+            analytical psychology. It says that a human life, like a day, has
+            its morning and its evening, each one with its tasks and goals.{' '}
           </Body>
         </Content>
       </Container>
-    </main>
-  </div>
+    </Main>
+  </>
 )
 
 export default Home
