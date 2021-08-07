740b322b07d0d4631850085be11b8b2c191ece11
Alan Morales <alanmoraales845@gmail.com>
2021-08-07 14:09:35 -0500

    add see all button to Section component

--- src/components/templates/Section/Section.tsx
+++ src/components/templates/Section/Section.tsx
@@ -1,15 +1,27 @@
 import { FC } from 'react'
 import { Grid } from '@chakra-ui/react'
 import { Heading } from 'components/atoms/Typography'
+import Flex from 'components/atoms/Flex'
+import GoToButton from 'components/atoms/Buttons/GoToButton'
 
 interface ISection {
   title: string
+  seeAllUrl?: string
+  seeAllLabel?: string
 }
 
-const Section: FC<ISection> = ({ title, children }) => (
+const Section: FC<ISection> = ({
+  title,
+  seeAllUrl,
+  seeAllLabel = 'See all',
+  children,
+}) => (
   <Grid as="section" gap={8} width="100%">
     <Heading variant="h6">{title}</Heading>
     {children}
+    <Flex direction="row-reverse">
+      <GoToButton href={seeAllUrl} label={seeAllLabel} />
+    </Flex>
   </Grid>
 )
 
