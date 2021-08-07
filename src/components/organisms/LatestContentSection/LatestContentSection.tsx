f50b9b366bf683ee8972a961097b09b81dfa58fb
Alan Morales <alanmoraales845@gmail.com>
2021-08-07 13:42:22 -0500

    add keys

--- src/components/organisms/LatestContentSection/LatestContentSection.tsx
+++ src/components/organisms/LatestContentSection/LatestContentSection.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react'
 import { Divider } from '@chakra-ui/react'
 import Section from 'components/templates/Section'
 import ArticleCard from 'components/molecules/ArticleCard'
@@ -6,10 +7,10 @@ import articles from 'data/articles'
 const LatestContentSection = () => (
   <Section title="Latest content">
     {articles.map((article) => (
-      <>
+      <Fragment key={article.slug}>
         <ArticleCard {...article} />
         <Divider />
-      </>
+      </Fragment>
     ))}
   </Section>
 )
