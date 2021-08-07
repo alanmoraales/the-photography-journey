18f8a26e59d05d84da37febf304415110280e440
Alan Morales <alanmoraales845@gmail.com>
2021-08-07 11:13:10 -0500

    using theme for gap property

--- src/components/atoms/Flex/Flex.tsx
+++ src/components/atoms/Flex/Flex.tsx
@@ -1,14 +1,20 @@
 import { FC } from 'react'
-import { Flex as ChakraFlex, FlexProps } from '@chakra-ui/react'
+import { Flex as ChakraFlex, FlexProps, useTheme } from '@chakra-ui/react'
 
 interface IFlex extends FlexProps {
-  gap?: string
+  gap?: string | number
 }
 
-const Flex: FC<IFlex> = ({ children, gap = '0', ...restOfProps }) => (
-  <ChakraFlex {...restOfProps} style={{ gap: gap }}>
-    {children}
-  </ChakraFlex>
-)
+const Flex: FC<IFlex> = ({ children, gap = '0', ...restOfProps }) => {
+  const theme = useTheme()
+  const themeGap = theme.space[gap]
+  const calculatedGap = themeGap || gap
+
+  return (
+    <ChakraFlex {...restOfProps} style={{ gap: calculatedGap }}>
+      {children}
+    </ChakraFlex>
+  )
+}
 
 export default Flex
