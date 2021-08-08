748ee61a36c1d3e147539ead8047c13a5b17b2c3
Alan Morales <alanmoraales845@gmail.com>
2021-08-08 01:09:29 -0500

    update image url

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -3,7 +3,7 @@ import { IArticle, EArticleType } from 'shared/types/article'
 const articles: IArticle[] = [
   {
     title: 'Starting The Journey',
-    coverUrl: 'https://i.ibb.co/6wDFLKQ/IMG-20201026-172930-3x.webp',
+    coverUrl: 'https://i.ibb.co/hCPt8fM/IMG-20201026-172930-1920.webp',
     type: EArticleType.Post,
     description:
       'Life, like a day, has a morning and an evening. In this article I want to introduce you The Two Halves Of Life and how this applies to the creative journey.',
