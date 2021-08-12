88a37b9ffd5d9f2623c347e17a5177b9c2b8e7ba
Alan Morales <alanmoraales845@gmail.com>
2021-08-11 21:39:28 -0500

    add JustifiedGallery component

--- src/components/organisms/GalleryPreviewSection/GalleryPreviewSection.tsx
+++ src/components/organisms/GalleryPreviewSection/GalleryPreviewSection.tsx
@@ -1,68 +1,14 @@
-import { useState } from 'react'
-import { Box } from '@chakra-ui/react'
-import useJustifiedLayout from 'use-justified-layout'
-import LazyImage from 'react-lazy-blur-image'
-import ContainerWithSizes, { ISizes } from 'components/atoms/ContainerWithSizes'
 import Section from 'components/templates/Section'
-import type { IImage } from 'shared/types/image'
+import JustifiedGallery from 'components/organisms/JustifiedGallery'
+import { GetComponentProps } from 'shared/types/common'
 
-interface IGalleryPreviewSection {
-  images: IImage[]
-}
+interface IGalleryPreviewSection
+  extends GetComponentProps<typeof JustifiedGallery> {}
 
-const GalleryPreviewSection = ({ images }: IGalleryPreviewSection) => {
-  const [galleryContainerWidth, setGalleryContainerWidth] = useState(0)
-  const [layout] = useJustifiedLayout({
-    layoutInput: images,
-    configuration: {
-      containerPadding: 0,
-      containerWidth: galleryContainerWidth,
-      showWidows: false,
-    },
-    dependencies: [galleryContainerWidth],
-  })
-  const { boxes, containerHeight } = layout
-
-  const onSizesChange = (sizes: ISizes) => {
-    setGalleryContainerWidth(sizes.width)
-  }
-
-  return (
-    <Section title="Gallery">
-      <Box width="100%" height={containerHeight}>
-        <ContainerWithSizes onSizeChange={onSizesChange}>
-          {boxes.map((box, index) => {
-            const { smallWebp, smallJpg, placeholder } = images[index]
-            const { left, width, height, top } = box
-            return (
-              <picture key={`${smallJpg}-${index}`}>
-                <source srcSet={smallWebp} />
-                <LazyImage
-                  placeholder={placeholder}
-                  uri={smallJpg}
-                  render={(src, style) => (
-                    <img
-                      loading="lazy"
-                      alt={smallWebp}
-                      src={src}
-                      width={width}
-                      height={height}
-                      style={{
-                        ...style,
-                        position: 'absolute',
-                        top,
-                        left,
-                      }}
-                    />
-                  )}
-                />
-              </picture>
-            )
-          })}
-        </ContainerWithSizes>
-      </Box>
-    </Section>
-  )
-}
+const GalleryPreviewSection = (props: IGalleryPreviewSection) => (
+  <Section title="Gallery">
+    <JustifiedGallery {...props} />
+  </Section>
+)
 
 export default GalleryPreviewSection
