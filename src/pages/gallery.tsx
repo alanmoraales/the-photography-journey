3d6a3dbfe360f894dc2cfd4c92a92ce15035a40b
Alan Morales <alanmoraales845@gmail.com>
2021-08-13 22:26:27 -0500

    using page template in gallery page

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,15 +1,10 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import styled from '@emotion/styled'
 import { Grid } from '@chakra-ui/react'
 import { getPlaiceholder } from 'plaiceholder'
-import Navbar from '@organisms/Navbar'
+import PageTemplate from '@templates/PageTemplate'
 import JustifiedGallery from '@organisms/JustifiedGallery'
 import Head from '@atoms/Head'
 import images from 'data/galleryImages'
-
-const Space = styled.div`
-  height: 32px;
-`
 
 export const getStaticProps: GetStaticProps = async () => {
   const imagesWithPlaceholder = await Promise.all(
@@ -33,20 +28,19 @@ const Home = ({ images }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <Head />
-      <Space />
-      <Navbar />
-      <Grid p={6} pt={8} placeItems="center">
-        <Grid
-          as="main"
-          gap={16}
-          placeItems="center"
-          maxWidth="container.lg"
-          width="100%"
-        >
-          <JustifiedGallery images={images} />
+      <PageTemplate>
+        <Grid p={6} pt={8} placeItems="center">
+          <Grid
+            as="main"
+            gap={16}
+            placeItems="center"
+            maxWidth="container.lg"
+            width="100%"
+          >
+            <JustifiedGallery images={images} />
+          </Grid>
         </Grid>
-      </Grid>
-      <Space />
+      </PageTemplate>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import styled from '@emotion/styled'
 import { Grid } from '@chakra-ui/react'
 import { getPlaiceholder } from 'plaiceholder'
-import Navbar from '@organisms/Navbar'
+import PageTemplate from '@templates/PageTemplate'
 import LatestContentSection from '@organisms/LatestContentSection'
 import GalleryPreviewSection from '@organisms/GalleryPreviewSection'
 import Head from '@atoms/Head'
 import images from 'data/galleryImages'
-
-const Space = styled.div`
-  height: 32px;
-`
 
 export const getStaticProps: GetStaticProps = async () => {
   const previewGalleryImages = await Promise.all(
@@ -36,29 +31,28 @@ const Home = ({
   return (
     <>
       <Head />
-      <Space />
-      <Navbar />
-      <Grid p={6} pt={8} placeItems="center">
-        <Grid
-          as="main"
-          gap={16}
-          placeItems="center"
-          maxWidth="container.lg"
-          width="100%"
-        >
+      <PageTemplate>
+        <Grid p={6} pt={8} placeItems="center">
           <Grid
-            w="100%"
-            templateColumns={{ base: 'auto', lg: '65% auto' }}
+            as="main"
             gap={16}
+            placeItems="center"
+            maxWidth="container.lg"
+            width="100%"
           >
-            <Grid gap={16}>
-              <LatestContentSection />
+            <Grid
+              w="100%"
+              templateColumns={{ base: 'auto', lg: '65% auto' }}
+              gap={16}
+            >
+              <Grid gap={16}>
+                <LatestContentSection />
+              </Grid>
             </Grid>
+            <GalleryPreviewSection images={previewGalleryImages} />
           </Grid>
-          <GalleryPreviewSection images={previewGalleryImages} />
         </Grid>
-      </Grid>
-      <Space />
+      </PageTemplate>
     </>
   )
 }
