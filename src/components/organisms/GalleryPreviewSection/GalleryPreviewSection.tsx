88c5318f3bcb3f601ea89ab884d9230a8ab8dd29
Alan Morales <alanmoraales845@gmail.com>
2021-08-13 22:25:57 -0500

    add link to gallery page

--- src/components/organisms/GalleryPreviewSection/GalleryPreviewSection.tsx
+++ src/components/organisms/GalleryPreviewSection/GalleryPreviewSection.tsx
@@ -1,12 +1,14 @@
 import Section from '@templates/Section'
 import JustifiedGallery from '@organisms/JustifiedGallery'
 import { GetComponentProps } from 'utils/common'
+import routes from '@constants/routes'
 
+const { gallery } = routes
 interface IGalleryPreviewSection
   extends GetComponentProps<typeof JustifiedGallery> {}
 
 const GalleryPreviewSection = (props: IGalleryPreviewSection) => (
-  <Section title="Gallery">
+  <Section title="Gallery" seeAllUrl={gallery}>
     <JustifiedGallery {...props} />
   </Section>
 )
