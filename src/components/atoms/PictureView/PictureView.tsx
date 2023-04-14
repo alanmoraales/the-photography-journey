44d43d5591c0e4b7c84c5b288ac5e322c0c52c25
alanmoraales <alanmoraales845@gmail.com>
2023-04-14 00:17:27 -0600

    improve gallery implementation

--- src/components/atoms/PictureView/PictureView.tsx
+++ src/components/atoms/PictureView/PictureView.tsx
@@ -1,7 +1,5 @@
-import { Box, Image } from '@chakra-ui/react'
-import VisibilitySensor from 'react-visibility-sensor'
-import ImagePlaceholder from '@atoms/ImagePlaceholder'
-import useShouldDisplayImage from 'hooks/useShouldDisplayImage'
+import { Box } from '@chakra-ui/react'
+import NextImage from 'next/image'
 import type { IImage } from '@declarations/image'
 
 interface IPictureView extends IImage {
@@ -10,58 +8,29 @@ interface IPictureView extends IImage {
 }
 
 const PictureView = ({
-  smallJpg,
-  smallWebp,
+  src,
   top,
   left,
   width,
   height,
-  css,
-}: IPictureView) => {
-  const { shouldDisplayImage, onImageLoaded, onImageVisibilityChange } =
-    useShouldDisplayImage()
-  const offset = -1000
-
-  return (
-    <VisibilitySensor
-      onChange={onImageVisibilityChange}
-      partialVisibility
-      resizeCheck
-      offset={{
-        top: offset,
-        bottom: offset,
-      }}
-    >
-      <Box
-        key={smallJpg}
-        position="absolute"
-        top={top}
-        left={left}
-        width={width}
-        height={height}
-      >
-        <ImagePlaceholder cssPlaceholder={css}>
-          <picture>
-            <source srcSet={smallWebp} />
-            <Image
-              loading="lazy"
-              onLoad={onImageLoaded}
-              alt={smallJpg}
-              src={smallJpg}
-              width={width}
-              htmlWidth={width}
-              height={height}
-              htmlHeight={height}
-              position="absolute"
-              top={0}
-              left={0}
-              opacity={shouldDisplayImage ? 1 : 0}
-            />
-          </picture>
-        </ImagePlaceholder>
-      </Box>
-    </VisibilitySensor>
-  )
-}
+  base64Placeholder,
+}: IPictureView) => (
+  <Box
+    key={src}
+    position="absolute"
+    top={top}
+    left={left}
+    width={width}
+    height={height}
+  >
+    <NextImage
+      alt={src}
+      src={src}
+      layout="fill"
+      placeholder="blur"
+      blurDataURL={base64Placeholder}
+    />
+  </Box>
+)
 
 export default PictureView
